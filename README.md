df32d7fe7999688a4e2ed76f55c2223e605c0180
uqrport1 <r.portas@uq.edu.au>
2015-12-02 14:30:52 +1000

    Added ability to run development server from gulp

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 The build tools use gulp, thus `node.js` is required.
 
-Once node.js is installed run `npm install` and then `gulp`
+Once node.js is installed run `npm install` and then `gulp` to build the project.
+
+Once built the files can be found under the directory named `build`
+
+To run the development server, run `gulp run`
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,24 @@
 var gulp = require('gulp');
 var download = require('gulp-download');
+var webserver = require('gulp-webserver');
 
 // Build Dependencies
 var browserify = require('gulp-browserify');
 var clean = require('gulp-clean');
 
-gulp.task('build', function() {
+gulp.task('build', ['clean'], function() {
     gulp.src('app/js/*')
         .pipe(gulp.dest('build/js'));
+
     gulp.src("app/img/*")
         .pipe(gulp.dest('build/img'));
 
     gulp.src("app/index.html")
-        .pipe(gulp.dest('build/index.html'));
+        .pipe(gulp.dest('build'));
     
 });
 
-gulp.task('download', function() {
+gulp.task('download', ['clean', 'build'], function() {
     // Download the threejs dependencies
     download("https://cdnjs.cloudflare.com/ajax/libs/three.js/r73/three.min.js")
         .pipe(gulp.dest('build/js/threejs'));
@@ -40,6 +42,13 @@ gulp.task('download', function() {
 gulp.task('clean', function() {
     return gulp.src('build', {read: false})
         .pipe(clean());
+});
+
+gulp.task('run', function() {
+    gulp.src('build')
+        .pipe(webserver({
+            open: true
+            }));
 });
 
 gulp.task('default', ['clean', 'build', 'download']);
